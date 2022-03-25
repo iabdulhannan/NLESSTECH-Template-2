1473326862a6cf34d5b303c7102d001d9a1c22e8
Abdul Hannan <74273970+iabdulhannan@users.noreply.github.com>
2022-03-25 05:26:57 +0500

    1- Developed services section 2- Made the homepage responsive

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      // screens: {
+      //   'tablet': '800px',
+        // => @media (min-width: 992px) { ... }
+      // },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
