0537cfb34342cb962eb3fabee6ac69c442ff3c7d
Abdul Hannan <74273970+iabdulhannan@users.noreply.github.com>
2022-07-15 14:45:59 +0500

    Minified TailwindCSS
    Updated caniuse-lite

--- Pages/Contact/contactForm.php
+++ Pages/Contact/contactForm.php
@@ -31,7 +31,7 @@ if (isset($_POST['sendMessage'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="../../Assets/Stylesheets/Tailwind.css" rel="stylesheet">
+    <link href="../../Assets/Stylesheets/TailwindMinified.css" rel="stylesheet">
     <link href="../../Assets/Stylesheets/index.css" rel="stylesheet">
     <script src="../../node_modules/jquery/dist/jquery.min.js"></script>
     <link href="Assets/contact.css" rel="stylesheet">
